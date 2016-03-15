c10b486b4df075209d7d554ef95f26a1e408420d
Zebadiah Ray Long <thegreatzebadiah@gmail.com>
2016-03-15 09:41:42 -0400

    Initial commit

---
+++ Assets/NATHelper.cs
@@ -0,0 +1,253 @@
+﻿using UnityEngine;
+using RakNet;
+using System;
+using System.Collections;
+
+/**
+ * Uses RakNet to perform NAT Punchthrough using an externally hosted Facilitator.
+ * Once a hole is punched a connection can be made via UNet HLAPI.
+ * Requires RakNet.dll and c# wrappers. Also requires the NATCompleteServer from 
+ * the RakNet samples to be running on an external server somewhere.
+ */
+public class NATHelper : MonoBehaviour
+{
+    // IP Addres and port of the server running NATCompleteServer
+    public string facilitatorIP = "50.57.111.104"; 
+    public ushort facilitatorPort = 61111;
+
+    [NonSerialized]
+    public string externalIP;
+    [NonSerialized]
+    public bool isReady;
+    [NonSerialized]
+    public string guid;
+
+    NatPunchthroughClient natPunchthroughClient;
+    SystemAddress facilitatorSystemAddress;
+    bool firstTimeConnect = true;
+    Action<int> onHolePunched;
+    RakPeerInterface rakPeer;
+
+    void Start()
+    {
+        rakPeer = RakPeerInterface.GetInstance();
+
+        StartCoroutine(connectToNATFacilitator());
+    }
+
+    /**
+     * Connect to the externally hosted NAT Facilitator (NATCompleteServer from the RakNet samples)
+     * This is called initially and also after each succesfull punchthrough received on the server.
+     */
+    IEnumerator connectToNATFacilitator()
+    {
+        // Start the RakNet interface listening on a random port
+        // We never need more than 2 connections, one to the Facilitator and one to either the server or the latest incoming client
+        // Each time a client connects on the server RakNet is shut down and restarted with two fresh connections
+        StartupResult startResult = rakPeer.Startup(2, new SocketDescriptor(), 1);
+        if (startResult != StartupResult.RAKNET_STARTED)
+        {
+            Debug.Log("Failed to initialize network interface: " + startResult.ToString());
+            yield break;
+        }
+        
+        // Connect to the Facilitator
+        ConnectionAttemptResult connectResult = rakPeer.Connect(facilitatorIP, (ushort)facilitatorPort, null, 0);
+        if (connectResult != ConnectionAttemptResult.CONNECTION_ATTEMPT_STARTED)
+        {
+            Debug.Log("Failed to initialize connection to NAT Facilitator: " + connectResult.ToString());
+            yield break;
+        }
+
+        // Connecting, wait for response
+        Packet packet;
+        while ((packet = rakPeer.Receive()) == null) yield return new WaitForEndOfFrame();
+        
+        // Was the connection accepted?
+        if (packet.data[0] != (byte)DefaultMessageIDTypes.ID_CONNECTION_REQUEST_ACCEPTED)
+        {
+            Debug.Log("Failed to connect to NAT Facilitator: " + ((DefaultMessageIDTypes)packet.data[0]));
+            yield break;
+        }
+
+        // Success, we are connected to the Facilitator
+        guid = rakPeer.GetMyGUID().g.ToString();
+        Debug.Log("Connected: " + guid);
+
+        // We store this for later so that we can tell which incoming messages are coming from the facilitator
+        facilitatorSystemAddress = packet.systemAddress;
+
+        // Now that we have an external connection we can get the externalIP
+        externalIP = rakPeer.GetExternalID(packet.systemAddress).ToString(false);
+
+        if (firstTimeConnect)
+        {
+            firstTimeConnect = false;
+
+            // Attach RakNet punchthrough client
+            // This is really what does all the heavy lifting
+            natPunchthroughClient = new NatPunchthroughClient();
+            rakPeer.AttachPlugin(natPunchthroughClient);
+            // Punchthrough can't happen until RakNet is done finding router port stride
+            // so we start it asap. If we didn't call this here RakNet would handle it
+            // when we actually try and punch through.
+            natPunchthroughClient.FindRouterPortStride(facilitatorSystemAddress);
+        }
+        else
+        {
+            // If this is not the first time connecting to the facilitor it means the server just received
+            // a successful punchthrough and it reconnecting to prepare for more punching. We can start
+            // listening immediately.
+            StartCoroutine(waitForIncomingNATPunchThroughOnServer(onHolePunched));
+        }
+
+        isReady = true;
+    }
+    
+    /**
+     * Called on the server to start listening for clients trying to punch through
+     */
+    public void startListeningForPunchthrough(Action<int> onHolePunched)
+    {
+        this.onHolePunched = onHolePunched;
+        rakPeer.SetMaximumIncomingConnections(2);
+        StartCoroutine(waitForIncomingNATPunchThroughOnServer(onHolePunched));
+    }
+
+    /**
+     * Wait for an incoming punchthrough from a client. Once a hole is punched
+     * RakNet is shut down so that the NetworkManager can create a new UNet Server
+     * listening on the port that the puncthrough arrived on.
+     */
+    IEnumerator waitForIncomingNATPunchThroughOnServer(Action<int> onHolePunched)
+    {
+        ushort natListenPort = 0;
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+
+            // Check for incoming packet
+            Packet packet = rakPeer.Receive();
+
+            // No packet, maybe next time
+            if (packet == null) continue;
+
+            // Got a packet, see what it is
+            RakNet.DefaultMessageIDTypes messageType = (DefaultMessageIDTypes)packet.data[0];
+            switch (messageType)
+            {
+                case DefaultMessageIDTypes.ID_NAT_PUNCHTHROUGH_SUCCEEDED:
+                    bool weAreTheSender = packet.data[1] == 1;
+                    if (!weAreTheSender)
+                    {
+                        // Someone successfully punched through to us
+                        // natListenPort is the port that the server should listen on
+                        natListenPort = rakPeer.GetInternalID().GetPort();
+                        // Now we're waiting for the client to try and connect to us
+                        Debug.Log("Received punch through from " + rakPeer.GetMyGUID() + " " + packet.systemAddress.ToString());
+                    }
+                    break;
+
+                case DefaultMessageIDTypes.ID_NEW_INCOMING_CONNECTION:
+                    // Cool we've got a connection. The client now knows which port to connect from / to
+                    Debug.Log("Received incoming RakNet connection.");
+                    // Close the connection to the client
+                    rakPeer.CloseConnection(packet.guid, true);
+                    // And also to the Facilitator
+                    rakPeer.CloseConnection(facilitatorSystemAddress, true);
+                    break;
+
+                case DefaultMessageIDTypes.ID_DISCONNECTION_NOTIFICATION:
+                    // Once the Facilitator has disconnected we shut down raknet
+                    // so that UNet can listen on the port that RakNet is currently listening on
+                    // At this point the hole is succesfully punched
+                    // RakNet is then reconnected to the facilitator on a new random port.
+                    if (packet.systemAddress == facilitatorSystemAddress)
+                    {
+                        rakPeer.Shutdown(0);
+                            
+                        // Hole is punched, UNet can start listening
+                        onHolePunched(natListenPort);
+
+                        // Reconnect to Facilitator for next punchthrough
+                        StartCoroutine(connectToNATFacilitator());
+                            
+                        yield break; // Totally done
+                    }
+                    break;
+
+                default:
+                    Debug.Log(((DefaultMessageIDTypes)packet.data[0]).ToString());
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Punch a hole form a client to the server identified by hostGUID
+     * Once the hole is punched onHolePunched will be called with the ports to use to connect
+     */
+    public void punchThroughToServer(string hostGUIDString, Action<int, int> onHolePunched)
+    {
+        RakNetGUID hostGUID = new RakNetGUID(ulong.Parse(hostGUIDString));
+        natPunchthroughClient.OpenNAT(hostGUID, facilitatorSystemAddress);
+        StartCoroutine(waitForResponseFromServer(hostGUID, onHolePunched));
+    }
+
+    /**
+     * Wait for a NAT Punchthrough responses from the server. Once a hole is punched
+     * RakNet is shutdown so the NetworkManager can connect via UNet
+     */
+    IEnumerator waitForResponseFromServer(RakNetGUID hostGUID, Action<int, int> onHolePunched)
+    {
+        ushort natListenPort = 0, natConnectPort = 0;
+        while (true)
+        {
+            Packet packet = rakPeer.Receive();
+            if (packet != null)
+            {
+                DefaultMessageIDTypes messageType = (DefaultMessageIDTypes)packet.data[0];
+
+                switch (messageType)
+                {
+                    case DefaultMessageIDTypes.ID_NAT_PUNCHTHROUGH_SUCCEEDED:
+                        // A hole has been punched but we're not done yet. We need to actually connect (via RakNet)
+                        // to the server so that we can get the port to connect from
+                        // natConnectPort is the external port of the server to connect to
+                        natConnectPort = packet.systemAddress.GetPort();
+                        rakPeer.Connect(packet.systemAddress.ToString(false), natConnectPort, "", 0);
+                        Debug.Log("Hole punched!" + packet.systemAddress.GetPort());
+                        break;
+
+                    case DefaultMessageIDTypes.ID_CONNECTION_REQUEST_ACCEPTED:
+                        // Ok, we connected, we can now use GetExternalID to get the port to connect from.
+                        natListenPort = rakPeer.GetExternalID(packet.systemAddress).GetPort();
+                        Debug.Log("Connected");
+                        // Now we wait for the server to disconnect us so that we know it is ready for the UNet connection
+                        break;
+
+                    case DefaultMessageIDTypes.ID_DISCONNECTION_NOTIFICATION:
+                        // Server has disconnected us. We are ready to connect via UNet
+
+                        // Shut down RakNet
+                        rakPeer.Shutdown(0);
+
+                        // Hole is punched!
+                        onHolePunched(natListenPort, natConnectPort);
+
+                        yield break; // Totally done
+
+                    default:
+                        Debug.Log("RakNet Client received unexpected message type: " + ((DefaultMessageIDTypes)packet.data[0]).ToString());
+                        break;
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    
+    void OnApplicationQuit()
+    {
+        rakPeer.Shutdown(0);
+    }
+}
---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using System.Reflection;
+
+public class NetworkManager : UnityEngine.Networking.NetworkManager
+{
+    NATHelper natHelper;
+    FieldInfo clientIDField;
+    NetworkMatch networkMatch;
+    string hostExternalIP, hostInternalIP;
+    List<NetworkServerSimple> natServers = new List<NetworkServerSimple>();
+
+    void Awake()
+    {
+        // We abuse unity's matchmaking system to pass around connection info but no match is ever actually joined
+        networkMatch = gameObject.AddComponent<NetworkMatch>();
+
+        // Use reflection to get a reference to the private m_ClientId field of NetworkClient
+        // We're going to need this later to set the port that client connects from.
+        // Even though it's called clientId it is actually the transport level host id that the NetworkClient
+        // uses to connect
+        clientIDField = typeof(NetworkClient).GetField("m_ClientId", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        natHelper = GetComponent<NATHelper>();
+    }
+    
+    public void OnGUI()
+    {
+        GUI.enabled = natHelper.isReady;
+        if (GUI.Button(new Rect(0, 10, 150, 100), "Host"))
+        {
+            // This is how RakNet identifies each peer in the network
+            // Clients will use the guid of the server to punch a hole
+            string guid = natHelper.guid;
+
+            // The easiest way I've found to get all the connection data to the clients is to just
+            // mash it all together in the match name
+            string name = string.Join(":", new string[] { natHelper.externalIP, Network.player.ipAddress, guid });
+            
+            networkMatch.CreateMatch(name, 2, true, "", natHelper.externalIP, Network.player.ipAddress, 0, 0, OnMatchCreate);
+        }
+        if (GUI.Button(new Rect(0, 120, 150, 100), "Join server"))
+        {
+            networkMatch.ListMatches(0, 1, "", true, 0, 0, OnMatchList);
+        }
+    }
+
+
+    #region Network events ------------------------------------------------------------------------
+
+    /**
+     * We start a host as normal but we also tell the NATHelper to start listening for incoming punchthroughs. 
+     */
+    public override void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        // It's important here that we host via NetworkServer.ListenRelay otherwise the match will
+        // be delisted from the UNet matchmaking service after 30 seconds and clients will not be
+        // able to find the game.
+        // Luckily this is exactly what unity does by default in OnMatchCreate so it's all good
+        base.OnMatchCreate(success, extendedInfo, matchInfo);
+
+        natHelper.startListeningForPunchthrough(OnHolePunchedServer);
+    }
+    
+    /**
+     * Punches a hole through to the host of the first match in the list
+     */
+    public override void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        if (!success || matchList.Count == 0) return;
+
+        string[] data = matchList[0].name.Split(':');
+        
+        hostExternalIP = data[0];
+        hostInternalIP = data[1];
+        string hostGUID = data[2];
+
+        natHelper.punchThroughToServer(hostGUID, OnHolePunchedClient);
+
+        // This would be a good time to try and make a direct connection without NAT Punchthrough
+        // If the connection succeeds then the code in OnHolePunchedClient isn't necessary
+    }
+
+    /**
+     * Server received a hole punch from a client
+     * Start up a new NATServer listening on the newly punched hole
+     */
+    void OnHolePunchedServer(int natListenPort)
+    {
+        NATServer newServer = new NATServer();
+
+        bool isListening = newServer.Listen(natListenPort, NetworkServer.hostTopology);
+        if (isListening)
+        {
+            natServers.Add(newServer);
+        }
+    }
+
+    /**
+     * Client punched through to a server
+     * Perform sacred ritual to create a custom client and attempt to connect
+     * through the hole.
+     */
+    void OnHolePunchedClient(int natListenPort, int natConnectPort)
+    {
+        // The port on the server that we are connecting to
+        networkPort = natConnectPort;
+
+        // Make sure to connect to the correct IP or things won't work
+        if (hostExternalIP == natHelper.externalIP)
+        {
+            if (hostInternalIP == Network.player.ipAddress)
+            {
+                // Host is running on the same computer as client, two separate builds
+                networkAddress = "127.0.0.1";
+            }
+            else
+            {
+                // Host is on the same local network as client
+                networkAddress = hostInternalIP;
+            }
+        }
+        else
+        {
+            // Host is somewhere out on the internet
+            networkAddress = hostExternalIP;
+        }
+        
+        Debug.Log("Attempting to connect to server " + networkAddress + ":" + networkPort);
+
+        // Standard client setup stuff than the NetworkMager would normally take care of for us
+        NetworkTransport.Init(globalConfig);
+        if (customConfig)
+        {
+            foreach (QosType type in base.channels)
+            {
+                connectionConfig.AddChannel(type);
+            }
+        }
+        else
+        {
+            connectionConfig.AddChannel(QosType.ReliableSequenced);
+            connectionConfig.AddChannel(QosType.Unreliable);
+        }
+
+        // If we try to use maxConnection when the Advanced Configuration checkbox is not checked
+        // we will get a crc mismatch because the host will have been started with the default
+        // max players of 8 rather than the value in maxConnections
+        int maxPlayers = 8;
+        if (customConfig)
+        {
+            maxPlayers = maxConnections;
+        }
+
+        HostTopology topo = new HostTopology(connectionConfig, maxPlayers);
+
+        // Start up a transport level host on the port that the hole was punched from
+        int natListenSocketID = NetworkTransport.AddHost(topo, natListenPort);
+
+        // Create and configure a new NetworkClient
+        client = new NetworkClient();
+        client.Configure(topo);
+
+        // Connect to the port on the server that we punched through to
+        client.Connect(networkAddress, networkPort);
+
+        // Magic! Set the client's transport level host ID so that the client will use
+        // the host we just started above instead of the one it creates internally when we call Connect.
+        // This has to be done so that the connection will be made from the correct port, otherwise
+        // Unity will use a random port to connect from and NAT Punchthrough will fail.
+        // This is the shit that keeps me up at night.
+        clientIDField.SetValue(client, natListenSocketID);
+
+        // Tell Unity to use the client we just created as _the_ client so that OnClientConnect will be called
+        // and all the other HLAPI stuff just works. Oh god, so nice.
+        UseExternalClient(client);
+    }
+
+    #endregion Network events ---------------------------------------------------------------------
+
+    void Update()
+    {
+        natServers.ForEach(server => server.Update());
+    }
+
+}
